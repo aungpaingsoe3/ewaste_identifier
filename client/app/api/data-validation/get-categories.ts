d25f5236f04aaa068dc8e9cb4531ca50a98b171b
Aung Paing Soe <aungpaing.soe@sjsu.edu>
2025-05-05 14:14:52 -0700

    Progress on Data Validation

--- client/app/api/data-validation/get-categories.ts
+++ client/app/api/data-validation/get-categories.ts
@@ -1,34 +1,42 @@
-export interface category {
-    topic: string
-    title: string;
-}
-export async function getCategories(): Promise<category[]> {
-    const url = 'https://www.ifixit.com/api/2.0/categories';
-    try {
-        const response = await fetch(url);
-        if (!response.ok){
-            throw new Error(`HTTP Error! Status: ${response.status}`);
+const IFIXIT_URL = "https://www.ifixit.com/api/2.0/categories";
+
+type Category = {
+    [key: string]: Category | null;
+};
+
+function flattenCategories(tree: Category, parent = ""): string[] {
+    const paths: string[] = [];
+    for (const key in tree){
+        const currentPath = parent ? `${parent}/${key}` : key;
+        paths.push(currentPath);
+
+        const value = tree[key];
+        if (value && typeof value == "object") {
+            paths.push(...flattenCategories(value, currentPath));
         }
-        const data = await response.json();
-        const categories: category[] = Object.values(data);
-        console.log(data);
-        return categories;
     }
-    catch (error) {
-        console.error("Fetching categories failed:", error);
-        return [];
+    return paths;
+}
+
+function isValidDevice(userInput: string, validDevices: string[][]): boolean {
+    const input = userInput.trim().toLowerCase();
+    return validDevices.some(path => path.includes(input));
+}
+
+async function runValidation(){
+    try {
+        const response = await fetch(IFIXIT_URL);
+        const categories: Category = await response.json();
+
+        const validDevices = flattenCategories(categories);
+        const validDevicesLowered = validDevices.map(path => path.split("/").map(segment => segment.toLowerCase()));
+
+        console.log(isValidDevice("Macbook Air", validDevicesLowered));
+        // console.log(isValidDevice("sagr", validDevicesLowered));
+    }
+    catch (error){
+        console.error("Error fetching or validing categories", error);
     }
 }
 
-async function filterCategories(input: string): Promise<category[]> {
-    const categories = await getCategories();
-
-    const filtered = categories.filter(category =>
-        category.title.toLowerCase().includes(input.toLowerCase())
-    );
-    return filtered;
-}
-
-filterCategories("Mac").then(filtered => {
-    console.log("Filtered Categories: ", filtered);
-});
+runValidation();
